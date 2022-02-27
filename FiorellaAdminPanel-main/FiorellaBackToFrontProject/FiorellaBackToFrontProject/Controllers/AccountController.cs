862c55f68fddfa783b9df339401d08ea70b7046d
Alisimran95 <alisimranem@code.edu.az>
2022-02-28 02:23:12 +0400

    UserInfoChange added

--- FiorellaAdminPanel-main/FiorellaBackToFrontProject/FiorellaBackToFrontProject/Controllers/AccountController.cs
+++ FiorellaAdminPanel-main/FiorellaBackToFrontProject/FiorellaBackToFrontProject/Controllers/AccountController.cs
@@ -14,6 +14,7 @@ namespace FiorellaBackToFrontProject.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        
 
         public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
         {
@@ -38,7 +39,7 @@ namespace FiorellaBackToFrontProject.Controllers
             var existUser = await _userManager.FindByNameAsync(registerViewModel.Username);
             if (existUser != null)
             {
-                ModelState.AddModelError("Username","There is user with this name ");
+                ModelState.AddModelError("Username", "There is user with this name ");
                 return View();
             }
 
@@ -50,18 +51,19 @@ namespace FiorellaBackToFrontProject.Controllers
 
             };
 
-            var result = await _userManager.CreateAsync(user,registerViewModel.Password);
+            var result = await _userManager.CreateAsync(user, registerViewModel.Password);
             if (!result.Succeeded)
             {
                 foreach (var error in result.Errors)
                 {
-                    ModelState.AddModelError("",error.Description);
+                    ModelState.AddModelError("", error.Description);
                 }
+
                 return View();
 
             }
 
-            await _signInManager.SignInAsync(user,false);
+            await _signInManager.SignInAsync(user, false);
 
             return RedirectToAction("Index", "Home");
         }
@@ -83,29 +85,31 @@ namespace FiorellaBackToFrontProject.Controllers
             var existUser = await _userManager.FindByNameAsync(loginViewModel.Username);
             if (existUser == null)
             {
-                ModelState.AddModelError("","Invalid Credentials");
+                ModelState.AddModelError("", "Invalid Credentials");
                 return View();
             }
 
-            var result = await _signInManager.PasswordSignInAsync(existUser,loginViewModel.Password,loginViewModel.RememberMe,false);
+            var result = await _signInManager.PasswordSignInAsync(existUser, loginViewModel.Password,
+                loginViewModel.RememberMe, false);
             if (result.IsLockedOut)
             {
-                ModelState.AddModelError("","Invalid Credentials");
+                ModelState.AddModelError("", "Invalid Credentials");
                 return View();
             }
 
             if (!result.Succeeded)
             {
-                ModelState.AddModelError("","Invalid Credentials");
+                ModelState.AddModelError("", "Invalid Credentials");
                 return View();
             }
 
             return RedirectToAction("Index", "Home");
         }
+
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult ResetPassword()
@@ -119,10 +123,11 @@ namespace FiorellaBackToFrontProject.Controllers
         {
             if (!ModelState.IsValid)
             {
-                ModelState.AddModelError("Email","Invalid Credentials");
+                ModelState.AddModelError("Email", "Invalid Credentials");
                 return View();
 
             }
+
             var existEmail = await _userManager.FindByEmailAsync(resetPasswordViewModel.Email);
             if (existEmail == null)
             {
@@ -131,7 +136,8 @@ namespace FiorellaBackToFrontProject.Controllers
             }
 
             string token = await _userManager.GenerateEmailConfirmationTokenAsync(existEmail);
-            string link = Url.Action(nameof(ChangePassword), "Account", new { email = resetPasswordViewModel.Email, token }, Request.Scheme, Request.Host.ToString());
+            string link = Url.Action(nameof(ChangePassword), "Account",
+                new {email = resetPasswordViewModel.Email, token}, Request.Scheme, Request.Host.ToString());
 
 
             MailMessage msg = new MailMessage();
@@ -143,7 +149,7 @@ namespace FiorellaBackToFrontProject.Controllers
             {
                 body = await reader.ReadToEndAsync();
             }
-
+             
             msg.Body = body.Replace("{{link}}", link);
             msg.Subject = "Verify";
             msg.IsBodyHtml = true;
@@ -164,18 +170,19 @@ namespace FiorellaBackToFrontProject.Controllers
 
         [HttpPost]
         [AutoValidateAntiforgeryToken]
-        public async Task<IActionResult> ChangePassword(ResetPasswordViewModel resetPasswordViewModel,ChangePasswordViewModel changePasswordViewModel)
+        public async Task<IActionResult> ChangePassword(ResetPasswordViewModel resetPasswordViewModel,
+            ChangePasswordViewModel changePasswordViewModel)
         {
             if (!ModelState.IsValid)
-            {   
-                ModelState.AddModelError("Password","invalid credentials");
+            {
+                ModelState.AddModelError("", "invalid credentials");
                 return View();
             }
 
             var existUser = await _userManager.FindByEmailAsync(resetPasswordViewModel.Email);
-            if (existUser==null)
+            if (existUser == null)
             {
-                ModelState.AddModelError("Username","User not found");
+                ModelState.AddModelError("", "User not found");
                 return View();
             }
 
@@ -185,6 +192,37 @@ namespace FiorellaBackToFrontProject.Controllers
             await _signInManager.RefreshSignInAsync(existUser);
 
             return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult ChangeUserPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> ChangeUserPassword(ChangeUserInfoViewModel changeUserInfoViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("","Invalid Credentials");
+                return View();
+            }
+
+            var existUser = await _userManager.GetUserAsync(User);
+
+            var password =  await _userManager.ChangePasswordAsync(existUser,changeUserInfoViewModel.OldPassword,changeUserInfoViewModel.NewPassword);
+           
+            if (!password.Succeeded)
+            {
+                ModelState.AddModelError("OldPassword","Old password isn't true");
+                return View();  
+            }
+            await _signInManager.RefreshSignInAsync(existUser);
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
---
+++ FiorellaAdminPanel-main/FiorellaBackToFrontProject/FiorellaBackToFrontProject/ViewModels/ChangeUserInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FiorellaBackToFrontProject.ViewModels
+{
+    public class ChangeUserInfoViewModel
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
